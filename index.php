f98a6d29c0261a73be44df21fc578ba0de25a789
drivedemon <superdriveza@hotmail.com>
2019-05-07 09:39:33 +0700

    + Add CSS animation login

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  	<link rel="stylesheet" href="/djopform/css/style.css" type="text/css">
     <title>Login</title>
     <style>
     th{
@@ -15,11 +16,19 @@
 
     </style>
 </head>
-<body>
-  <img src="./img/network-1.jpg" width="100%" height="125">
+<body class="animate">
+  <!-- <img src="./img/network-1.jpg" width="80%" height="100"> -->
 <div style="display: flex; justify-content: center; padding-top: 100px">
-    <form name="frmlogin"  method="post" action="login.php">
-        <table cellspacing="20" >
+    <form class="box" name="frmlogin"  method="post" action="login.php">
+
+
+        <h1>ลงชื่อเข้าใช้</h1>
+        <input type="text" name="txtUser" placeholder="Username">
+        <input type="password" name="txtPass" placeholder="Password">
+        <input type="submit" value="Login">
+
+
+        <!-- <table cellspacing="20" >
             <tr align="center" ><th colspan ="2">ลงชื่อเข้าใช้</th></tr>
             <tr>
                 <td cellspacing="10"> ชื่อผู้ใช้ </td>
@@ -32,7 +41,7 @@
             <tr><td align="center" colspan ="2"><button type="submit">Login</button>
                 &nbsp;&nbsp;
                 <button type="reset">Reset</button></td></tr>
-        </table>
+        </table> -->
     </form>
 </body>
 </html>
