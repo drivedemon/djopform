dea660d0f50d6e16d72e80fc4a6636af6d01d27e
drivedemon <superdriveza@hotmail.com>
2019-05-07 10:15:57 +0700

    + Add BG CSS

--- index.php
+++ index.php
@@ -18,30 +18,15 @@
 </head>
 <body class="animate">
   <!-- <img src="./img/network-1.jpg" width="80%" height="100"> -->
-<div style="display: flex; justify-content: center; padding-top: 100px">
+    <div class="stars"></div>
+    <div class="twinkling"></div>
+    <div class="clouds"></div>
     <form class="box" name="frmlogin"  method="post" action="login.php">
-
-
         <h1>ลงชื่อเข้าใช้</h1>
         <input type="text" name="txtUser" placeholder="Username">
         <input type="password" name="txtPass" placeholder="Password">
         <input type="submit" value="Login">
-
-
-        <!-- <table cellspacing="20" >
-            <tr align="center" ><th colspan ="2">ลงชื่อเข้าใช้</th></tr>
-            <tr>
-                <td cellspacing="10"> ชื่อผู้ใช้ </td>
-                <td> : <input type="text" id="Username" name="txtUser" placeholder="Username"required></td>
-            </tr>
-            <tr>
-                <td> รหัสผ่าน </td>
-                <td> : <input type="password" id="Password"  name="txtPass" placeholder="Password"required></td>
-            </tr>
-            <tr><td align="center" colspan ="2"><button type="submit">Login</button>
-                &nbsp;&nbsp;
-                <button type="reset">Reset</button></td></tr>
-        </table> -->
     </form>
+
 </body>
 </html>
